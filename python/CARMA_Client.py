ef6a590553392b04b3cdcb1e1f927c0cfeb76b29
rodot- <jto22@drexel.edu>
2016-05-13 16:47:32 -0400

    Added a function to get a specific LC

--- python/CARMA_Client.py
+++ python/CARMA_Client.py
@@ -1,9 +1,4 @@
-import zmq, sys, os, time, cPickle, StringIO, numpy as np, re, random
-import carmcmc as cm
-import multiprocessing as mp
-from JacksTools import jio
-
-
+import zmq
 
 def getRandLC():
 
@@ -15,6 +10,18 @@ def getRandLC():
 	fname, z, data = socket.recv_pyobj()
 	socket.close()
 	return fname, z, data
+
+def getLC(ID):
+
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.connect('tcp://76.124.106.126:5001')
+
+	socket.send(b"getLC\n%s" % ID)
+	fname, z, data = socket.recv_pyobj()
+	socket.close()
+	return fname, z, data
+
 
 if __name__ == '__main__':
 
